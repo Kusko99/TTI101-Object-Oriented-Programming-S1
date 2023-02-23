873aeeb1ad0507aeed037fc21be04097067f91f5
Kusko99 <99877193+Kusko99@users.noreply.github.com>
2023-02-23 19:21:18 -0300

    Add files via upload

---
+++ Apostila05/Naturais100For.java
@@ -0,0 +1,7 @@
+public class Naturais100For {
+    public static void main(String[] args) {
+        for(int i=1; i < 101; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Apostila05/Naturais100ForV2.java
@@ -0,0 +1,7 @@
+public class Naturais100ForV2 {
+    public static void main(String[] args) {
+        for(int i=100; i > 0; i--){
+            System.out.println(i);
+        }
+    }
+}
